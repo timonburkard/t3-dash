b1681976712da7c492994b2d28e67eb4d5b02b56
Timon Burkard <burkard.timon@gmail.com>
2026-05-14 13:14:32 +0200

    [fw/nucleo] simple test blinking LED tasks
    - M4 blinks LD1
    - M7 blinks LD2
    
    Signed-off-by: Timon Burkard <burkard.timon@gmail.com>

--- fw/nucleo/CM4/app/app.c
+++ fw/nucleo/CM4/app/app.c
@@ -2,8 +2,24 @@
 
 #include <stdbool.h>
 
+#include "FreeRTOS.h"
+#include "main.h"
+#include "task.h"
+
+void task_led(void* params)
+{
+    while (true) {
+        HAL_GPIO_TogglePin(LD1_GPIO_Port, LD1_Pin);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
 void app(void)
 {
+    xTaskCreate(task_led, "LED Task", 512, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
     while (true) {
     }
 }
