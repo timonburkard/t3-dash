2220aa424d5c82c9b594867b11f528b569016f9a
Timon Burkard <burkard.timon@gmail.com>
2026-05-15 11:09:39 +0200

    [fw/nucleo] added dummy acquisition task logic
    It produces dummy water temperature and battery voltage values.
    
    In the future, this will be changed so that the acquisition tasks
    just triggers the data acquisition. E.g., for analog values: start
    the ADC with interrupt, in the ISR the `queue_data_raw` will be filled.
    
    Signed-off-by: Timon Burkard <burkard.timon@gmail.com>

--- fw/nucleo/CM4/app/acquisition/acquisition.c
+++ fw/nucleo/CM4/app/acquisition/acquisition.c
@@ -3,6 +3,8 @@
 #include <stdbool.h>
 
 #include "FreeRTOS.h"
+#include "app.h"
+#include "queue.h"
 #include "task.h"
 
 void acquisition_task(void* params)
@@ -14,6 +16,16 @@ void acquisition_task(void* params)
         // Start all sensor acquisitions in parallel, TODO:
         // HAL_ADC_Start_IT(&hadc1); // Water temperature
         // HAL_ADC_Start_IT(&hadc2); // Battery voltage
+
+        sensor_data_t water_temperature = {.id = SENSOR_ID_WATER_TEMPERATURE, .value = 123};
+        sensor_data_t battery_voltage   = {.id = SENSOR_ID_BATTERY_VOLTAGE, .value = 567};
+
+        if (xQueueSend(queue_data_raw, &water_temperature, portMAX_DELAY) != pdPASS) {
+            while (true) {} // TODO: Error handling
+        }
+        if (xQueueSend(queue_data_raw, &battery_voltage, portMAX_DELAY) != pdPASS) {
+            while (true) {} // TODO: Error handling
+        }
 
         vTaskDelayUntil(&last_wakeup, pdMS_TO_TICKS(1000));
     }
